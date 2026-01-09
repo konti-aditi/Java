7803f30442c97af14ba995c22e7b2a6246a0c882
Aditi <aditis.github@gmail.com>
2026-01-09 16:21:26 +0530

    var1

---
+++ var1.java
@@ -0,0 +1,35 @@
+class var1 
+{
+	public static void main(String[] args) 
+	{
+		String name;
+		int sal;
+		char grade;
+		byte age;
+		long phno;
+		float cgpa;
+		boolean isAlive;
+		String degree;
+		
+		name="Aditi Shinde" ; 
+		sal = 72000;
+		grade = 'A' ;
+		age = 0 ;
+		phno = 9876543219l;
+		cgpa= 9.89f;
+		isAlive =  true;
+		degree = "BE";
+		
+		System.out.println(name);
+		System.out.println(sal);
+		System.out.println(grade);
+		System.out.println(age);
+		System.out.println(phno);
+		System.out.println(cgpa);
+		System.out.println(isAlive);
+		System.out.println(degree);
+		
+		
+		
+	}
+}
